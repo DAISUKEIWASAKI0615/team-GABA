6210f5b35cfbee6fa47945be94caf7f22baabc45
DAISUKEIWASAKI0615 <1816004@s.asojuku.ac.jp>
2020-05-11 15:37:54 +0900

    5/11追加

---
+++ モナーブラザーズ/モナーブラザーズ/main.cpp
@@ -0,0 +1,221 @@
+#include "Dxlib.h"
+#include <math.h>
+#include "main.h"
+#include "keycheck.h"
+
+#define COLOR_MODE 16
+
+typedef enum {
+	GMODE_INIT,
+	GMODE_TITLE,
+	GMODE_GAME,
+	GMODE_GAMEOVER
+}GMODE;
+
+GMODE gamemode;
+int gameCounter;
+int fadeCnt;
+bool fadeIn;
+bool fadeOut;
+bool pause;														//一時停止フラグ
+int blocks[15];
+
+bool SysInit(void);
+void GameInit(void);
+void GameTitle(void);
+void GameMain(void);
+void GameOver(void);
+void GameMainDraw(void);
+void GameTitleDraw(void);
+void GameOverDraw(void);
+bool FadeInScreen(int fadeStep);
+bool FadeOutScreen(int fadeStep);
+void HitCheck(void);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	if (SysInit() == false)
+	{
+		return -1;
+	}
+
+	gamemode = GMODE_INIT;
+
+	//ｹﾞｰﾑﾙｰﾌﾟ
+	while (ProcessMessage() == 0
+		&& CheckHitKey(KEY_INPUT_ESCAPE) == 0)
+	{
+		ClsDrawScreen();
+		KeyCheck();
+		switch (gamemode)
+		{
+		case GMODE_INIT:
+			GameInit();
+			gamemode = GMODE_TITLE;
+			break;
+		case GMODE_TITLE:
+			GameTitle();
+			if (fadeIn)
+			{
+				if (!FadeInScreen(5)) fadeIn = false;
+			}
+			else if (fadeOut)
+			{
+				if (!FadeOutScreen(5))
+				{
+					gamemode = GMODE_GAME;
+					fadeOut = false;
+					fadeIn = true;
+				}
+			}
+			else
+			{
+				if (trgKey[START]) fadeOut = true;
+			}
+			//if (trgKey[START])
+			//{
+			//	gamemode = GMODE_GAME;
+			//}
+			break;
+		case GMODE_GAME:
+			GameMain();
+			if (fadeIn)
+			{
+				if (!FadeInScreen(5)) fadeIn = false;
+			}
+			break;
+		case GMODE_GAMEOVER:
+			GameOver();
+			if (fadeOut)
+			{
+				if (!FadeOutScreen(5))
+				{
+					gamemode = GMODE_INIT;
+					fadeOut = false;
+					fadeIn = true;
+				}
+			}
+			else
+			{
+				if (trgKey[START]) fadeOut = true;
+			}
+			break;
+		default:
+			break;
+		}
+		if (!pause) gameCounter++;
+		ScreenFlip();
+	}
+	DxLib_End;
+	return 0;
+}
+
+bool SysInit(void)
+{
+	//-----------ｼｽﾃﾑ処理
+	SetWindowText(_T("モナーブラザーズ"));
+	//640x480ﾄﾞｯﾄ65536色ﾓｰﾄﾞに設定 //8ﾋﾞｯﾄは全て足すと0～255の256種類
+	SetGraphMode(SCREEN_SIZE_X, SCREEN_SIZE_Y, 16);
+	ChangeWindowMode(true);																//true:window　false:ﾌﾙｽｸﾘｰﾝ
+
+	if (DxLib_Init() == -1)																//DXﾗｲﾌﾞﾗﾘの初期化 //DxLibを使った画像の読み込み等はこのﾌﾟﾛｸﾞﾗﾑのあとに
+	{
+		return false;																	//処理内容 //returnは関数を抜けるときに使われる
+	}																					//ifの式はここまででﾜﾝｾｯﾄ
+	pause = false;
+	fadeIn = false;
+	fadeOut = false;
+	KeyCheckSystemInit();
+	return true;
+}
+
+void GameInit(void)
+{
+	LoadDivGraph(_T("画像/ブロック群.png"), 15, 5, 3, 64, 64,blocks);
+}
+
+void GameTitle(void)
+{
+	GameTitleDraw();
+	DrawGraph(100, 100, blocks[0], false);
+}
+
+void GameTitleDraw(void)
+{
+	DrawString(0, 0, _T("GameTitle"), 0xFFFFFF);
+}
+
+void GameMain(void)
+{
+	if (trgKey[PAUSE])
+	{
+		pause = !pause;
+	}
+	if (pause)
+	{
+		SetDrawBright(128, 128, 128);
+	}
+	else
+	{
+		HitCheck();
+		if (trgKey[START]) gamemode = GMODE_GAMEOVER;
+	}
+	GameMainDraw();
+	if (pause)
+	{
+		SetDrawBright(255, 255, 255);
+		DrawString(360, 292, _T("PAUSE"), 0xFFFFFF);
+	}
+}
+
+void GameMainDraw(void)
+{
+	DrawFormatString(0, 0, 0xFFFFFF, _T("GameMain:%d"), gameCounter);
+}
+
+void GameOver(void)
+{
+	GameOverDraw();
+}
+
+void GameOverDraw(void)
+{
+	DrawString(0, 0, _T("GameOver"), 0xFFFFFF);
+}
+
+bool FadeInScreen(int fadeStep)
+{
+	fadeCnt += fadeStep;
+	if (fadeCnt <= 255)
+	{
+		SetDrawBright(fadeCnt, fadeCnt, fadeCnt);
+		return true;
+	}
+	else
+	{
+		SetDrawBright(255, 255, 255);
+		fadeCnt = 0;
+		return false;
+	}
+}
+
+bool FadeOutScreen(int fadeStep)
+{
+	fadeCnt += fadeStep;
+	if (fadeCnt <= 255)
+	{
+		SetDrawBright(255 - fadeCnt, 255 - fadeCnt, 255 - fadeCnt);
+		return true;
+	}
+	else
+	{
+		SetDrawBright(0, 0, 0);
+		fadeCnt = 0;
+		return false;
+	}
+}
+
+void HitCheck(void)
+{
+
+}
---
+++ モナーブラザーズ/モナーブラザーズ/main.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define SCREEN_SIZE_X 800
+#define SCREEN_SIZE_Y 600
+
+#define P1 3.141592
+
+typedef struct {
+	float x;
+	float y;
+}XY;
+
+typedef struct {
+	int x;
+	int y;
+}xy;
